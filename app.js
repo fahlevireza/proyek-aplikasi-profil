5915244848379a5504dd256053c6f8fdb040f96d
fahlevireza <fahlevireza2022@gmail.com>
2022-05-31 13:54:54 +0700

    submission gcp 1

--- app.js
+++ app.js
@@ -9,16 +9,8 @@ router.get('/', function (req, res) {
   //__dirname : It will resolve to your project folder.
 });
 
-router.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname + '/about.html'));
-});
-
-router.get('/sitemap', function (req, res) {
-  res.sendFile(path.join(__dirname + '/sitemap.html'));
-});
-
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 8000);
 
-console.log('Running at Port 3000');
+console.log('Running at Port 8000');
