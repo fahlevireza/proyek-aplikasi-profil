c8533a338894b06e1f7f9fe9c457b55b55af037f
fahlevireza2022 <fahlevireza2022@gmail.com>
2022-06-02 08:41:53 +0000

    submission1

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+const { format } = require('util');
 const express = require('express');
 const app = express();
+const Multer = require('multer');
 const path = require('path');
-const router = express.Router();
 app.use(express.static(__dirname + '/public'));
 
-router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-  //__dirname : It will resolve to your project folder.
+const { Storage } = require('@google-cloud/storage');
+
+const storage = new Storage();
+
+app.set('view engine', 'pug');
+
+app.use(express.json());
+
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
 });
 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 8000);
+const bucket = storage.bucket('process.env.submission-project1.appspot.com');
 
-console.log('Running at Port 8000');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+const PORT = parseInt(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
